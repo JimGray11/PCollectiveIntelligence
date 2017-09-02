98c510f30774345699ed3a1c560b63d17115f5ec
JimGray11 <ywendeng@126.com>
2017-09-02 20:45:46 +0800

    层次聚类实现
    1. 在层次聚类中将两个点的距离计算之后缓存---使用两点的ID tuple 作为key ，减少计算次数
    2. 使用二叉树的结构来保存节点

--- DiscoveringGroups/h_clustering.py
+++ DiscoveringGroups/h_clustering.py
@@ -29,7 +29,7 @@ def read_file(file_name):
         while line:
             p = line.split("\t")
             rows_name.append(p[0])
-            data.append(map(int,p[1:-1]))
+            data.append(map(int, p[1:-1]))
             line = fr.readline()
     return columns_name, data, rows_name
 
@@ -48,10 +48,57 @@ def pearson(v1, v2):
     den = sqrt((sum1Sq - pow(sum1, 2) / len(v1)) * (sum2Sq - pow(sum2, 2) / len(v1)))
     if den == 0:
         return 0
-    return 1-num / den
+    return 1 - num / den
+
+
+# 使用层次聚类
+def hcluster(data_set, distance=pearson):
+    # 使用一个map将两个点之间的距离计算一次之后就缓存起来
+    distances = {}
+    # 新形成的点id为负
+    cluster_id = -1
+    # 1.初始化每个点都作为一个簇
+    clust = [bi_cluster(data_set[i], i) for i in range(len(data_set))]
+    # 开始进行循环遍历进行层次聚类
+    while len(clust) > 1:
+        # 2.开始计算两个点之间的距离，并选择最小的两个点
+        loest_pair = (0, 1)
+        cloest = distance(clust[0].vec, clust[1].vec)
+        distances[(clust[0].id, clust[1].id)] = cloest
+        for i in range(len(clust)):
+            for j in range(i + 1, len(clust)):
+                if (clust[i].id, clust[j].id) not in distances:
+                    # 如果两个点之间的距离不存在，则将两点之间的距离加入结合中
+                    distances[(clust[i].id, clust[j].id)] = distance(clust[i].vec, clust[j].vec)
+                d = distances[(clust[i].id, clust[j].id)]
+                if d < cloest:
+                    cloest = d
+                    loest_pair = (i, j)
+        # 3.更新簇的中心点
+        center_ver = [(p1 + p2) / 2 for p1, p2 in zip(clust[loest_pair[0]].vec,
+                                                      clust[loest_pair[1]].vec)]
+        cluster_center = bi_cluster(center_ver, cluster_id,
+                                    clust[loest_pair[0]], clust[loest_pair[1]], cloest)
+        cluster_id -= 1
+        # 4.并更新簇节点(删除聚类最近的两个节点,添加新的簇中心)
+        # 注意由于下标的变化，需要先删除1，再删除0
+        del clust[loest_pair[1]]
+        del clust[loest_pair[0]]
+
+        clust.append(cluster_center)
+    return clust[0]
+
+
+def roate_matrix(data_set):
+    roate_data = []
+    for i in range(len(data_set[0])):
+        # 注意普通二维数组的遍历和numpy、DataFrame多维数组遍历的区别
+        obj = [data_set[j][i] for j in range(len(data))]
+        roate_data.append(obj)
+    return roate_data
 
 
 if __name__ == '__main__':
     columns, data, rows = read_file("data_set/blogdata.txt")
-
-     pearson(data[1], data[2])
+    hcluster(data)
+    roate= roate_matrix(data)
